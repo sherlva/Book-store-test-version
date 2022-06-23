f390e2f3a9c0a271718afa65599623bdc0ddac6a
Sherzod Akhmadjonov <97534012+sherlva@users.noreply.github.com>
2022-06-23 15:57:16 +0500

    feat: add a new files

---
+++ model/Book_Mongo.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose')
+
+const bookSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    }
+})
+
+
+module.exports = model('Book', bookSchema)
